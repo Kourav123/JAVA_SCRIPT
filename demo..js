6fc6dd37d41e782041dcc613166e3b27cd5643d8
ashish <ashishkourav3@gmail.com>
2023-09-23 17:33:45 +0530

    Java Script code demo commit

---
+++ demo..js
@@ -0,0 +1,3 @@
+let hello;
+hello = 'how are you!';
+alert(hello);
---
+++ script.js
@@ -0,0 +1,15 @@
+
+var hello;
+hello = "Hello User!! Welcome to XYZ Page";
+alert(hello);
+
+let std1Name = "Jack";
+alert(std1Name);
+function say(){
+    var hello;
+    hello = "Hello User!! Welcome to XYZ Page";
+    const avgHeight = "Average Man Height is 177cm";
+
+    alert(hello);
+    alert(avgHeight);
+}
